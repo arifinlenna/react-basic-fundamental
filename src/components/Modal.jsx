5855ecfa2973eda69d2c46267d3264f14653b71b
arifinlenna <arifin@lenna.ai>
2023-07-02 18:29:53 +0700

    adding codition in modal.jsx

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -24,7 +24,7 @@ export default function Modal(params) {
         {
             params.show === true &&
             <div>
-                { loading &&
+                { loading ?
                 <div className="m-auto z-1 h-100 w-100 position-fixed d-block">
                     <div className="card overflow-auto h-100 w-100" style={{ backgroundColor: "rgba(0,0,0,0.4)" }}>
                         <div className="card-body w-50 m-auto">
@@ -36,23 +36,19 @@ export default function Modal(params) {
                             />
                         </div>
                     </div>
-                </div>
-                    
-                }
-                {
-                    !loading &&
-                    <div className="m-auto z-1 h-100 w-100 position-fixed d-block" onClick={params.onClick}>
-                        <div className="card overflow-auto h-100 w-100" style={{ backgroundColor: "rgba(0,0,0,0.4)" }}>
-                            <div className="card-body w-50 m-auto">
-                            <ul className="list-group list-group-flush"  onClick={insideContent}>
-                                <li className="list-group-item">{params.user.name}</li>
-                                <li className="list-group-item">{params.user.username}</li>
-                                <li className="list-group-item">{params.user.email}</li>
-                                <li className="list-group-item">{params.user.phone}</li>
-                            </ul>
-                            </div>
+                </div> :
+                <div className="m-auto z-1 h-100 w-100 position-fixed d-block" onClick={params.onClick}>
+                    <div className="card overflow-auto h-100 w-100" style={{ backgroundColor: "rgba(0,0,0,0.4)" }}>
+                        <div className="card-body w-50 m-auto">
+                        <ul className="list-group list-group-flush"  onClick={insideContent}>
+                            <li className="list-group-item">{params.user.name}</li>
+                            <li className="list-group-item">{params.user.username}</li>
+                            <li className="list-group-item">{params.user.email}</li>
+                            <li className="list-group-item">{params.user.phone}</li>
+                        </ul>
                         </div>
                     </div>
+                </div>
                 }
             </div>
         }
